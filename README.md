68e9a1a2f29d108352d3a074326bdbe942acab60
Tinywan <756684177@qq.com>
2017-06-12 11:33:16 +0800

    定义独立的配置目录

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # thinkphp5-study-line
 thinkphp5-study-line
+
+##  定义独立的配置目录 
+##  定义独立的配置目录 
---
+++ application/config/index/database.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tinywan
+ * Date: 2017/6/12
+ * Time: 11:13
+ * Mail: Overcome.wan@Gmail.com
+ */
