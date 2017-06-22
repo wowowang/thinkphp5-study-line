39dbf695eb8827c1fa5caaa8679d57e9eb759b6d
ShaoBo Wan <overcome.wan@gmail.com>
2017-06-22 22:39:27 +0800

    update wechat branch helper.php

--- application/helper.php
+++ application/helper.php
@@ -27,3 +27,14 @@ function get_uri($url)
     //显示获得的数据
     return $response;
 }
+
+function getJson($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return json_decode($output, true);
+}
