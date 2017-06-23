524f220c22d1377108588a174abede7b720d230e
ShaoBo Wan <overcome.wan@gmail.com>
2017-06-23 22:39:36 +0800

    :revolving_hearts: :revolving_hearts: :revolving_hearts: :revolving_hearts:  微信配置信息

---
+++ config/extra/wechat.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tinywan
+ * Date: 2017/6/23
+ * Time: 22:36
+ * ****************************微信配置信息**********************************
+ */
+return [
+    "appid" => 'wx94c43716d8a91f3f',
+    "appsecret" => 'd4624c36b6795d1d99dcf0547af5443d'
+];
